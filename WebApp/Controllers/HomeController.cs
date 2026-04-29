10462bf1b691cadb105942420f0b3e312e689be2
Edgar Yair Cauich Canul <23470127@campeche.tecnm.mx>
2026-04-28 20:39:05 -0600

    Muestra Informacion de una coleccion usando una tabla html

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -8,18 +8,70 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
-            Proyector proyector = new Proyector()
+            var modelo = LoadData();
+            //var modelo= new LinkedList<Proyector>();
+
+            return View(modelo);
+        }
+        private IEnumerable<Proyector> LoadData()
+        {
+            var proyectores = new List<Proyector>();
+
+            proyectores.Add(new Proyector()
             {
-                id = 2,
+                id = 1,
                 Marca = "Epson",
-                Modelo = "ligth",
-                NumeroDeSerie = "123456",
+                Modelo = "Star",
+                NumeroDeSerie = "4324324",
                 Situacion = SituacionProyector.Bueno,
                 FechDeAlta = DateTime.Now,
                 FechDeBaja = DateTime.Now
-            };
+            });
 
-            return View(proyector);
+            proyectores.Add(new Proyector()
+            {
+                id = 2,
+                Marca = "Hp",
+                Modelo = "Ultra",
+                NumeroDeSerie = "123456",
+                Situacion = SituacionProyector.Regular,
+                FechDeAlta = DateTime.Now,
+                FechDeBaja = DateTime.Now
+            });
+
+            proyectores.Add(new Proyector()
+            {
+                id = 3,
+                Marca = "Luna",
+                Modelo = "ligth",
+                NumeroDeSerie = "14326",
+                Situacion = SituacionProyector.Malo,
+                FechDeAlta = DateTime.Now,
+                FechDeBaja = DateTime.Now
+            });
+
+            proyectores.Add(new Proyector()
+            {
+                id = 4,
+                Marca = "Epson",
+                Modelo = "Gold",
+                NumeroDeSerie = "1375",
+                Situacion = SituacionProyector.Bueno,
+                FechDeAlta = DateTime.Now,
+                FechDeBaja = DateTime.Now
+            });
+
+            proyectores.Add(new Proyector()
+            {
+                id = 5,
+                Marca = "Epson",
+                Modelo = "ligth",
+                NumeroDeSerie = "123456",
+                Situacion = SituacionProyector.Regular,
+                FechDeAlta = DateTime.Now,
+                FechDeBaja = DateTime.Now
+            });
+            return proyectores;
         }
 
         public IActionResult Privacy()
