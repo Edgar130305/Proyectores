27cb6559aca5dbc69f8ec3b676182a92fc184746
Edgar Yair Cauich Canul <23470127@campeche.tecnm.mx>
2026-05-08 11:21:12 -0600

    Realiza validaciones usando DataAnnotations

--- WebApp/Models/Proyector.cs
+++ WebApp/Models/Proyector.cs
@@ -1,14 +1,21 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Proyector
     {
         public int id { get; set; }
+        [Required(ErrorMessage ="Debe proporcionar un valor para el campo Marca")]
+        [StringLength(24)]
         public string Marca { get; set; }
+        [Required(ErrorMessage = "Debe proporcionar un valor para el campo Modelo")]
         public string Modelo { get; set; }
+        [Required(ErrorMessage = "Debe proporcionar un valor para el campo Numero de Serie")]
         public string NumeroDeSerie { get; set; }
-        public SituacionProyector Situacion { get; set; }
-        public DateTime FechDeAlta { get; set; }
-        public DateTime FechDeBaja { get; set; }
+        public SituacionProyector Situacion { get; set; } 
+            = SituacionProyector.Bueno;
+        public DateTime FechDeAlta { get; set; } = DateTime.Now;
+        public DateTime? FechDeBaja { get; set; }
 
 
     }
